9eaaba4e705ed75aa0e075b8eb647f1ce00ab65d
HamaHs <lusuy1995@gmail.com>
2023-09-24 20:51:27 +0200

    added script for procesing of loging page

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,36 @@
 'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+   const signInForm = document.querySelector('form');
+
+   if (signInForm) {
+      const username = signInForm.querySelector('#username');
+      const password = signInForm.querySelector('#password');
+
+      signInForm.addEventListener('submit', (e) => {
+         e.preventDefault();
+
+         username.classList.remove('shake-animate', 'alert');
+         password.classList.remove('shake-animate', 'alert');
+
+         const formData = Object.fromEntries(new FormData(signInForm).entries());
+
+         fetch('http://localhost:3000/users')
+            .then(response => response.json())
+            .then(({ admins, registeredUsers }) => {
+               admins.forEach(admin => {
+                  if (admin.login === formData.username && admin.password === formData.password) {
+                     window.location.replace('http://localhost:3001/');
+                  } else {
+                     username.classList.add('shake-animate', 'alert');
+                     password.classList.add('shake-animate', 'alert');
+                  }
+               });
+            })
+            .finally(() => {
+               signInForm.reset();
+            });
+      });
+   }
+
+});
